14d64d5e52c0673b57a11c03b2519f499f0351b2
Seaver Olson <sjolson007@gmail.com>
2025-10-03 17:07:28 -0500

    added instruction memory implementation ebcause memory2c was way too much

---
+++ src/instructionMemory.v
@@ -0,0 +1,19 @@
+//memory2c had a lot of overhead as it accounted for writing as well however other than the inital load of instructions instruction memory should be read only
+module instructionMemory(
+    input wire [31:0] readAddress, //hook to PC later
+    output reg [31:0] instruction
+);
+    reg [31:0] memory [0:16383];//64K bytes of memory
+
+    initial begin
+        integer i;
+        for (i = 0; i < 16384; i=i+1) begin
+            memory[i] = 32'd0;
+        end
+        $readmemh("loadfile_all.img", memory);
+    end
+
+    always @(*) begin
+        instruction = memory[readAddress[15:2]]; //word - 2 bit offset
+    end
+endmodule
