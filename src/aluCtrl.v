bf29a1787221bf4fccaaf1cff60c443230d7de5f
Seaver Olson <sjolson007@gmail.com>
2025-10-07 23:56:13 -0500

    finished alu Control Unit

--- src/aluCtrl.v
+++ src/aluCtrl.v
@@ -6,17 +6,26 @@ module aluControl(
 ); 
     always @(*) begin
         case (ALUOp) 
-            2'b00: begin
-                ALUControl = 4'b0010;
+            //LOAD and STORE
+            2'b00: begin ALUControl = 4'b0010; end
+            // BRANCH
+            2'b01: begin ALUControl = 4'b0011; end
+
+            2'b10: begin
+                case (funct3)
+                    3'b000: begin ALUControl = (funct7) ? 4'b0110 : 4'b0010; end
+                    3'b001: begin ALUControl = 4'b0100; end
+                    3'b010: begin ALUControl = 4'b0111; end
+                    3'b011: begin ALUControl = 4'b1000; end
+                    3'b100: begin ALUControl = 4'b0011; end
+                    3'b101: begin ALUControl = (funct7) ? 4'b1001 : 4'b0101; end
+                    3'b110: begin ALUControl = 4'b0001; end
+                    3'b111: begin ALUControl = 4'b0000; end
+                    default: begin ALUControl = 4'b0010; end
+                endcase
             end
 
-            2'b01: begin
-                ALUControl = 4'b0011;
-            end
-
-            default: begin
-                ALUControl = 4'b0010;//for testing ADD if fail
-            end
+            default: begin ALUControl = 4'b0010; end
         endcase 
     end
  
